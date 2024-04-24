a2c2ee29e1ec3dd010c9ebeaec9f85c171ee9a73
NFLandgraf <nicolas.landgraf@web.de>
2024-04-24 18:42:45 +0200

    distances

--- Analysis.py
+++ Analysis.py
@@ -1,5 +1,5 @@
 #%% HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
-# Lets keep df as the main DataFrame, where the index is the frame number. 
+# Lets keep main_df as the main DataFrame, where the index is the frame number. 
 # If we calculate sth new (e.g. animal center, static_paw), there is the function fuse_dfs() to add new columns to the df. 
 # Whenever we work on/manipulate the df, lets do this with a df.copy(), so the df is untouched
 
@@ -16,6 +16,8 @@ from matplotlib import pyplot as plt
 df = pd.read_csv('data\\2m_292_crop.csv', header=None)
 
 x_pixel, y_pixel = 570, 570
+arena_length = 400 #mm
+px_per_mm = np.mean([x_pixel, y_pixel]) / arena_length
 fps = 100
 all_bodyparts = ['snout', 'mouth', 'paw_VL','paw_VR', 'middle', 'paw_HL', 'paw_HR','anus', 'tail_start', 'tail_middle', 'tail_end']
 colors = ['b', 'g', 'r', 'c', 'm', 'y']
@@ -25,20 +27,19 @@ colors = ['b', 'g', 'r', 'c', 'm', 'y']
 #%% HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
 # FUNCTIONS
 
-# FUSE 2 dfs
-def fuse_dfs(df1, df2):
-    
-    # reindex df2 to the index of df1
-    df2 = df2.reindex(df1.index)
 
-    # add df2 to df1
+def fuse_dfs(df1, df2):
+    # FUSE 2 dfs
+    # reindex df2 to the index of df1 (important if you have nan gaps)
+    df2 = df2.reindex(df1.index)
     df1 = df1.join(df2)
 
     return df1
 
-# you have a series with conditions (e.g. True/False or float/nan) in each row
-# return the start & stop frame of the [cond_onset, cond_offset]
+
 def timestamps_onsets(series, min_frame_nb, onset_cond=float):
+    # you have a series with conditions (e.g. True/False or float/nan) in each row
+    # return the start & stop frame of the [cond_onset, cond_offset]
     timestamp_list = []
     start_idx = None
     prev_idx = None
@@ -108,33 +109,87 @@ def cleaning_raw_df(df):
         df.loc[filter, f'{bodypart}_y'] = np.nan
         df = df.drop(labels=f'{bodypart}_likelihood', axis="columns")
     
+    # add a gaussian filter over the df to reduce outlayers, don't know how necessary this is
+    df = df.rolling(window=10, win_type='gaussian', min_periods=1, center=True, axis=1).mean(std=1)
+
     return df
 
+
+
+def point2centerline_dist(df, point, centerline_bps):
+
+    # get all 'pos' float values (not nan) in the frame_window ad add index to list
+    pos_in_static = df.loc[static_window[0] : static_window[1], f'{midline}_x'].copy().dropna()
+    pos_in_static_idx = pos_in_static.index.tolist()
+
+
+    # if there are no pos_coords in the frames_window of static_feet, add a middle frame
+    if len(pos_in_static_idx) == 0:
+        pos_in_static_idx.append(int(np.mean(static_window)))
+
+    # if only 2 or less pos_coords, add one above and below until we have >= 3
+    while len(pos_in_static_idx) < 3:
+        pos_in_static_idx = add_higherlower_float_row(df, pos_in_static_idx, f'{midline}_x')
+
+    print(f'relevant_pos_frames: {pos_in_static_idx}')
+
+
 # get the center of the animal via mean(stable bodyparts) and create new dataframe, also skip frames
-def animal_center(df, frames2skip):
+def add_columns_to_df(df, px_per_mm):
 
-    # only includes every k-th row to reduce noise
-    df_crop = df.iloc[::frames2skip]
+    new_df = pd.DataFrame()
 
+    # create time series for center that is represented by the mean of stable bodyparts
     # choose bodyparts that are always tracked
-    stable_bodyparts = ['middle', 'paw_HL', 'paw_HR', 'anus', 'tail_start']
+    stable_bodyparts = ['middle', 'anus', 'tail_start']
     stable_bodyparts_x = [f'{bodypart}_x' for bodypart in stable_bodyparts]
     stable_bodyparts_y = [f'{bodypart}_y' for bodypart in stable_bodyparts]
 
     # per row, take the mean of the stable bodyparts to have a stable animal position time series
-    row_means_x = df_crop[stable_bodyparts_x].mean(axis="columns")
-    row_means_y = df_crop[stable_bodyparts_y].mean(axis="columns")
+    new_df['pos_x'] = df[stable_bodyparts_x].mean(axis="columns")
+    new_df['pos_y'] = df[stable_bodyparts_y].mean(axis="columns")
 
-    # create a new data frame with animal position
-    df_center = pd.DataFrame(data={'pos_x': row_means_x, 'pos_y':row_means_y})
-
-    # add a gaussian filter over the time series to reduce outlayers, don't know how necessary this is
-    df_center = df_center.rolling(window=10, win_type='gaussian', min_periods=1, center=True).mean(std=1)
+    # add distances beetween bps    
+    new_df['dist_H_paws'] = euclidian_dist(df['paw_HL_x'].values, df['paw_HL_y'].values, df['paw_HR_x'].values, df['paw_HR_y'].values, px_per_mm)
+    new_df['dist_V_paws'] = euclidian_dist(df['paw_VL_x'].values, df['paw_VL_y'].values, df['paw_VR_x'].values, df['paw_VR_y'].values, px_per_mm)
+    new_df['dist_L_paws'] = euclidian_dist(df['paw_HL_x'].values, df['paw_HL_y'].values, df['paw_VL_x'].values, df['paw_VL_y'].values, px_per_mm)
+    new_df['dist_R_paws'] = euclidian_dist(df['paw_HR_x'].values, df['paw_HR_y'].values, df['paw_VR_x'].values, df['paw_VR_y'].values, px_per_mm)
     
+    # takes bps that would represent an intrinsic centerline, calculates linear regression line for each row and adds columns for slope and intercept
+    # takes nan if less than 2 bps without nan
+    def linear_fit(row, x_headers, y_headers):
+        # fits a linear regression line to the column values provided and returns the coefficients
+        x = row.loc[x_headers]
+        y = row.loc[y_headers]
+
+        if np.sum(~np.isnan(x)) < 2:
+            return (np.nan, np.nan)
+        
+        coeffs = np.polyfit(x, y, 1)
+        return coeffs
+    
+    centerline_x_headers = ['snout_x', 'mouth_x', 'middle_x', 'anus_x', 'tail_start_x']
+    centerline_y_headers = ['snout_y', 'mouth_y', 'middle_y', 'anus_y', 'tail_start_y']
+    new_df[['centerline_slopes', 'centerline_intercepts']] = df.apply(linear_fit, args=(centerline_x_headers, centerline_y_headers), axis=1, result_type='expand')
+    
+    
+    
+    
+    new_df['dist_HL2center'] = euclidian_dist(df['paw_HL_x'].values, df['paw_HL_y'].values, new_df[#NO; USE CENTERLINE].values, new_df['pos_y'].values, px_per_mm)
+    new_df['dist_HR2center'] = euclidian_dist(df['paw_HR_x'].values, df['paw_HR_y'].values, new_df[#NO; USE CENTERLINE].values, new_df['pos_y'].values, px_per_mm)
+    new_df['dist_VL2center'] = euclidian_dist(df['paw_VL_x'].values, df['paw_VL_y'].values, new_df[#NO; USE CENTERLINE].values, new_df['pos_y'].values, px_per_mm)
+    new_df['dist_VR2center'] = euclidian_dist(df['paw_VR_x'].values, df['paw_VR_y'].values, new_df[#NO; USE CENTERLINE].values, new_df['pos_y'].values, px_per_mm)
+
+
+
     # add df_center to df
     df = fuse_dfs(df, df_center)
 
     return df
+
+
+
+
 
 # get list of start- and stop-frames, where paws are static and df with only the mean paw position, rest is nan
 def static_feet(df, min_paw_movement, min_static_frame_nb):
@@ -354,4 +409,18 @@ staticpaw2midline_dist(main_df, static_window, 'pos')
 # %%
 
 
+def euclidian_dist(bp1xVals, bp1yVals, bp2xVals, bp2yVals, px_per_mm):
+    # calculates the euclidian distance between 2 bps in mm
+    series = (np.sqrt((bp1xVals - bp2xVals) ** 2 + (bp1yVals - bp2yVals) ** 2)) / px_per_mm
 
+    return series
+
+
+
+def add_distances_to_df(df,px_per_mm):
+
+    
+
+
+
+    
